714c586304e13a23707ce2a0a3b9d503a13c031d
Amith A G <amithanil32@gmail.com>
2023-09-23 20:40:35 +0530

    intial

---
+++ src/schema/zodSchema.ts
@@ -0,0 +1,37 @@
+import * as z from "zod";
+
+const fclSchema = z.object({
+  from: z.string().min(1),
+  to: z.string().min(1),
+  modes: z.literal("FCL"),
+  f_quantity: z.coerce.number().min(1).max(32767),
+  f_weight: z.coerce.number().min(1).max(32767),
+  comment: z.string().min(3).max(160).optional(),
+});
+
+const lclSchema = z.object({
+  from: z.string().min(1),
+  to: z.string().min(1),
+  modes: z.literal("LCL"),
+  l_container_type: z.enum(["pallete", "boxes", "package", "bag"]),
+  l_loading: z.string().min(1),
+  comment: z.string().min(3).max(160),
+});
+
+const bulkSchema = z.object({
+  from: z.string().min(1),
+  to: z.string().min(1),
+  modes: z.literal("BULK"),
+  b_loading_rate: z.coerce.number().min(1),
+  b_discharge_rate: z.coerce.number().min(1),
+  comment: z.string().min(3).max(160),
+});
+
+export const FormSchema = z.discriminatedUnion("modes", [
+  fclSchema,
+  lclSchema,
+  bulkSchema,
+]);
+
+
+
